4ca8a7b2214c59cb54dda51b1e47e195e5667347
MirzaShahzadSaleem <MirzaShahzadSaleem>
2021-09-21 04:48:56 -0700

    first Commit

---
+++ todo-app/src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h1> TODOS LIST</h1>
+        </div>
+    )
+}
+
+export default Header;
